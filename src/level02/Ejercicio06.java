c35ee95cc8e05130f23656ba062a543b1d703548
Federico117 <96803062+Federico117@users.noreply.github.com>
2022-07-14 08:26:26 -0500

    agregamos el Ejercicio06 con la clase Empleado

--- src/level02/Ejercicio06.java
+++ src/level02/Ejercicio06.java
@@ -9,12 +9,13 @@ public class Ejercicio06 {
 	public static void main(String[] args) {
 		Set<Empleado> setEmpleado = new HashSet<>();
 		Map<Integer, Double> dniSueldo = new HashMap<>();
+		//agregamos los empleados al hashset
 		setEmpleado.add(new Empleado("Federico Valdez", 40605106, 8, 500));
 		setEmpleado.add(new Empleado("Carlos Valdez", 40605107,  8, 800));
 		setEmpleado.add(new Empleado("Maximiliano Valdez", 24325761, 6, 1000));
 		setEmpleado.add(new Empleado("Ezequiel Valdez", 24325762, 8, 900));
 		setEmpleado.add(new Empleado("Juan Valdez", 24306405, 3, 1500));
-		//procedemos a calcular el sueldo y meterlo a un map
+		//procedemos a calcular el sueldo de cada empleado y almacenarlo en map con la clave del dni
 		for(Empleado emp: setEmpleado){
 			double sueldox = emp.getHorasTrabajadas() * emp.getValorHora();
 			dniSueldo.put(emp.getDni(), sueldox);
