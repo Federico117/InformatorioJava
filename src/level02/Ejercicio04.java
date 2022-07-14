cbcf670998fb0ba4b279b7084dd30a64f6740e9b
Federico117 <96803062+Federico117@users.noreply.github.com>
2022-07-14 07:48:08 -0500

    Update Ejercicio04.java

--- src/level02/Ejercicio04.java
+++ src/level02/Ejercicio04.java
@@ -1,10 +1,31 @@
 package level02;
 
-public class Ejercicio04 {
+import java.util.ArrayList;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        List<String> alumnos = new ArrayList<>();
+        alumnos.add("Federico");
+        alumnos.add("Susana");
+        alumnos.add("Nicolas");
+        alumnos.add("Marcela");
+        alumnos.add("Adrian");
+        alumnos.add("Marta");
+        alumnos.add("Marcelo");
+        alumnos.add("Mariana");
+        alumnos.add("Pablo");
+        alumnos.add("Mirta");
+        alumnos.add("Mario");
+        alumnos.add("Veronica");
+        //separamos alumnos por cursos
+        var cursoDesarrolloWeb = alumnos.subList(0, 4);
+        var cursoTesting = alumnos.subList(4, 8);
+        var cursoDisenioUX = alumnos.subList(8, 12);
+        System.out.println("Alumnos del curso de Desarrollo: "+cursoDesarrolloWeb);
+        System.out.println("Alumnos del curso de Testing: "+cursoTesting);
+        System.out.println("Alumnos del curso de Diseño UX: "+cursoDisenioUX);
+	    
+    }
+	
 }
